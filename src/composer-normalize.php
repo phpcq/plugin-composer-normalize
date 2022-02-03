8bf22eef35aec7f2cb9ef302b1a67ec3a6c51094
David Molineus <david.molineus@netzmacht.de>
2022-02-03 14:37:59 +0100

    Fix regular expressions

--- src/composer-normalize.php
+++ src/composer-normalize.php
@@ -118,9 +118,9 @@ return new class implements DiagnosticsPluginInterface {
                     private const REGEX_NOT_NORMALIZED = '#^.* is not normalized\.$#';
                     private const REGEX_IS_NORMALIZED = '#^.* is already normalized\.$#';
                     private const REGEX_XDEBUG_ENABLED = '#^(?<message>Composer is operating slower than normal' .
-                    ' because you have Xdebug enabled\. See https://getcomposer.org/xdebug)$#';
+                    ' because you have Xdebug enabled\. See https://getcomposer\.org/xdebug)$#';
                     private const REGEX_CURL_DISABLED = '#^(?<message>Composer is operating significantly slower' .
-                    ' than normal because you do not have the PHP curl extension enabled.$#';
+                    ' than normal because you do not have the PHP curl extension enabled\.)$#';
                     private const REGEX_LOCK_OUTDATED = '#^(?<message>The lock file is not up to date with the latest' .
                     ' changes in composer\.json, it is recommended that you run `composer update --lock`\.)$#';
                     private const REGEX_SCHEMA_VIOLATION = '#^.* does not match the expected JSON schema:$#';
